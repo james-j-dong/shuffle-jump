60d3cfb5a8513e759dded3421ad9887d3bea665d
James Dong <jamesjd1024@gmail.com>
2025-02-12 21:36:55 -0600

    div wrapper for player

--- src/components/MediaPlayer.tsx
+++ src/components/MediaPlayer.tsx
@@ -5,6 +5,7 @@ function MediaPlayer() {
             <div className="relative w-[270px] h-[584px] overflow-hidden rounded-[2.5rem] shadow-xl drop-shadow-lg border border-gray-700 bg-black"
                 style={{ boxShadow: "0 5px 30px rgba(0, 0, 0, 0.5)" }}
             >
+                <div className = "w-full h-full overflow-hidden rounded-[2.5rem]">
                 <ReactPlayer 
                     url="https://www.youtube.com/shorts/_OEiBtO3E9U" 
                     controls 
@@ -12,8 +13,9 @@ function MediaPlayer() {
                     loop
                     width="100%"
                     height="100%"
-                    className="video-player"
                 />
+                </div>
+
             </div>
         </div>
     );
