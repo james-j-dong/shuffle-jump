522f87604d38029d10358ed910148c64551b3f15
James Dong <jamesjd1024@gmail.com>
2025-02-14 20:47:53 -0600

    iframe improvement

--- src/components/MediaPlayer.tsx
+++ src/components/MediaPlayer.tsx
@@ -1,24 +1,24 @@
+import React from "react";
 import ReactPlayer from "react-player";
-function MediaPlayer() {
+
+function MediaPlayer({ url = "https://www.youtube.com/shorts/mcHUXUgHu7E" }) {
     return (
-        <div className="flex items-center justify-center min-h-screen p-4">
-            <div className="relative w-[270px] h-[584px] overflow-hidden rounded-[2.5rem] shadow-xl drop-shadow-lg border border-gray-700 bg-black"
+        <div className="flex items-center justify-center w-full h-full p-4">
+            <div 
+                className="relative aspect-[9/19.5] w-[270px] max-w-[90vw] overflow-hidden rounded-[2.5rem] shadow-xl border border-gray-700 bg-black"
                 style={{ boxShadow: "0 5px 30px rgba(0, 0, 0, 0.5)" }}
             >
-                <div className = "w-full h-full overflow-hidden rounded-[2.5rem]">
                 <ReactPlayer 
-                    url="https://www.youtube.com/shorts/_OEiBtO3E9U" 
+                    url={url}
                     controls 
                     playing
                     loop
                     width="100%"
                     height="100%"
                 />
-                </div>
-
             </div>
         </div>
     );
 }
 
-export default MediaPlayer
+export default MediaPlayer;
